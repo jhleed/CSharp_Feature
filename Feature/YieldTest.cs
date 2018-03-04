9c84affb8a14a85c4e80f8b7b2aff4f9f8ca597f
jongholee <jongholee@ebay.com>
2018-03-04 22:11:22 +0900

    [C#] yield : Sample1 : print 찍어서 확

---
+++ Feature/YieldTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Feature
+{
+	[TestClass]
+	public class YieldTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			foreach (var num in GetNumber())
+			{
+				Console.WriteLine(num);
+			}
+		}
+
+
+		private static IEnumerable<int> GetNumber()
+		{
+			yield return 10;  // 첫번째 루프에서 리턴되는 값
+			yield return 20;  // 두번째 루프에서 리턴되는 값
+			yield return 30;  // 세번째 루프에서 리턴되는 값
+		}
+
+	}
+}
